426e13ebf91d52ae63f824414fcf38fcb6f62573
XuanNam58 <104215250+XuanNam58@users.noreply.github.com>
2025-07-10 15:04:37 +0700

    create classroom service

---
+++ classroom-service/src/main/java/com/example/classroom_service/repository/AssignmentRepository.java
@@ -0,0 +1,7 @@
+package com.example.classroom_service.repository;
+
+import com.example.assignment_service.entity.Assignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssignmentRepository extends JpaRepository<Assignment, Integer> {
+}
