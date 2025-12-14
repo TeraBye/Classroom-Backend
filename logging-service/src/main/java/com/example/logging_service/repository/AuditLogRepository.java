06d7f407b16d69c3817b1fdc58c7097441d88586
XuanNam58 <104215250+XuanNam58@users.noreply.github.com>
2025-12-14 10:59:46 +0700

    create get audit log api

--- logging-service/src/main/java/com/example/logging_service/repository/AuditLogRepository.java
+++ logging-service/src/main/java/com/example/logging_service/repository/AuditLogRepository.java
@@ -2,8 +2,9 @@ package com.example.logging_service.repository;
 
 import com.example.logging_service.entity.AuditLog;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-    
+    Page<AuditLog> findByUsername(String username, Pageable pageable);
 }
